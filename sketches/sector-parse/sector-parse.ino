43e65cd305f67ee8b3a99ff2f4f5d09312486aa6
chillbuilds <will@chillbuilds.com>
2020-08-09 23:37:04 -0600

    sector parse cleanup

--- sketches/sector-parse/sector-parse.ino
+++ sketches/sector-parse/sector-parse.ino
@@ -4,7 +4,6 @@
 #include "Keyboard.h"
 #define READ_AS_NDEF (1)
 
-// define pins for SPI communication
 #define PN532_SCK  (2)
 #define PN532_MOSI (3)
 #define PN532_SS   (4)
@@ -24,8 +23,7 @@ void setup(void) {
   if (! versiondata) {
     Serial.print("pn532 not found");
     while (1); // halt
-  }else{
-  Serial.println("ready to read..");}
+  }else{Serial.println("ready to read..");}
 
   nfc.SAMConfig();
 }
@@ -44,49 +42,28 @@ void loop(void) {
 
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   if (success) {
-//    int uidStore[] = {uid[0], uid[1], uid[2], uid[3]};
-//    int xm1[] = {5, 37, 237, 60};
-//    for(int i = 0; i < 3; i++){
-//    if (uidStore[i] != xm1[i]){
-//      return;
-//    }
-//    }
+
     if (uidLength == 4)
     {
-      // Now we try to go through all 16 sectors (each having 4 blocks)
-      // authenticating each sector, and then dumping the blocks
       for (currentblock = sector*4; currentblock < (sector*4)+4; currentblock++)
       {
-        // Check if this is a new block so that we can reauthenticate
         if (nfc.mifareclassic_IsFirstBlock(currentblock)) authenticated = false;
-
-        // If the sector hasn't been authenticated, do so first
+        
         if (!authenticated)
         {
-          // Starting of a new sector ... try to to authenticate
           if (currentblock == 0)
-          {
-              success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, key);
-          }
+          {success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, key);}
           else
-          {
-              success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, key);
-          }
-          if (success)
-          {
+          {success = nfc.mifareclassic_AuthenticateBlock (uid, uidLength, currentblock, 1, key);}
+          if (success){
             authenticated = true;
-          }
-          else
-          {
+          }else{
             // trigger led
           }
         }
-        // If we're still not authenticated just skip the block
         if (!authenticated){
-          // trigger authentication error alert (after you make one up)          
           return;}
         else{
-          // Dump the data into the 'data' array
           success = nfc.mifareclassic_ReadDataBlock(currentblock, data);
           if (success)
           {
@@ -103,6 +80,9 @@ void loop(void) {
   for(int i = 0; i < 64; i++){
     int holder = sectorArr[i];
     if(holder == -2 && active == true){
+      delay(500);
+      Keyboard.press(KEY_RETURN);
+      Keyboard.release(KEY_RETURN);
       delay(2500);
       return;
     }else
@@ -113,8 +93,4 @@ void loop(void) {
     Keyboard.print(sectorArr[i]);
     }
   }
-  delay(600);
-//  Keyboard.press(KEY_RETURN);
-//  Keyboard.release(KEY_RETURN);
-  delay(2500);
 } 
